da90c3f548ee50f73837a5301bc754811072fd0c
Leo Von Randow <47364609+leovonrandow@users.noreply.github.com>
2022-07-25 10:50:21 -0300

    Personagens withhooks
    Coloquei os personagens em component hooks.

--- src/TRABALHOREACT/PERSONAGENS/PERSONAGENS.js
+++ src/TRABALHOREACT/PERSONAGENS/PERSONAGENS.js
@@ -1,56 +1,106 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PersonagemDetail from './PersonagemDetail/PersonagemDetail'
 import './PERSONAGENS.scss'
 
-export default class Personagens extends React.Component {
+//useMemo/useCallback
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            personagens: [],
-            personagensOriginal: []
-        };
-    }
+export default function Personagens() {
+  // Declare uma nova variável de state, a qual chamaremos de "count"
+  const [personagens, setPersonagens] = useState([]);
+  const [personagensOriginal, setPersonagensOriginal] = useState([]);
 
-    componentDidMount() {
-        fetch('https://swapi.dev/api/people/')
-            .then(res => res.json())
-            .then(res => {
-                this.setState({
-                    personagens: res.results,
-                    personagensOriginal: res.results
-                });
-            });
-    }
+  useEffect(() => {
+    fetch('https://swapi.dev/api/people/')
+      .then(res => res.json())
+      .then(res => {
+        setPersonagens(res.results)
+        setPersonagensOriginal(res.results)
+      });
+  }, []);
 
-    pesquisar(texto) {
-        const { personagensOriginal} = this.state;
-        const dadosFiltrados = personagensOriginal.filter((personagens) =>
-           (personagens.name.toLowerCase().includes(texto.toLowerCase()))
-        )
-        this.setState({
-            personagens: dadosFiltrados
-        })
-    }
+  const pesquisar = (texto) => {
+    // Executa o setState puxando o ultimo valor salvo no estado
+    // setPersonagens((prevState) => {
+    //   return prevState.filter(p => p.name.toLowerCase().includes(texto.toLowerCase()))
+    // });
 
-    render() {
-        const { personagens } = this.state;
-        return (
-            <div className='boxpesquisa'>
-                <label>PESQUISAR</label>
-                <input className='designImput' onChange={(e) => this.pesquisar(e.target.value)} />
-   
-                <h1>Lista de Personagens:</h1>
+    const dadosFiltrados = personagensOriginal.filter(p => p.name.toLowerCase().includes(texto.toLowerCase()));
+    setPersonagens(dadosFiltrados);
+  }
 
-                <ul className="personagens">
-                    {personagens.map((item) => (
-                        <PersonagemDetail
-                            key={item.people}
-                            personagem={item}
-                        />
-                    ))}
-                </ul>
-            </div>
-        );
-    }
+
+  // function getPersonagens() {}
+  // const getPersonagens = () => { }
+
+  return (
+    <div className='boxpesquisa'>
+      <label>PESQUISAR</label>
+      <input className='designImput' onChange={(e) => pesquisar(e.target.value)} />
+
+      <h1>Lista de Personagens:</h1>
+
+      <ul className="personagens">
+        {personagens.map((item) => (
+          <PersonagemDetail
+            key={item.people}
+            personagem={item}
+          />
+        ))}
+      </ul>
+    </div>
+  );
 }
+
+
+// export default class Personagens extends React.Component {
+
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       personagens: [],
+//       personagensOriginal: []
+//     };
+//   }
+
+//   componentDidMount() {
+//     fetch('https://swapi.dev/api/people/')
+//       .then(res => res.json())
+//       .then(res => {
+//         this.setState({
+//           personagens: res.results,
+//           personagensOriginal: res.results
+//         });
+//       });
+//   }
+
+//   pesquisar(texto) {
+//     const { personagensOriginal } = this.state;
+//     const dadosFiltrados = personagensOriginal.filter((personagens) =>
+//       (personagens.name.toLowerCase().includes(texto.toLowerCase()))
+//     )
+//     this.setState({
+//       personagens: dadosFiltrados
+//     })
+//   }
+
+//   render() {
+//     const { personagens } = this.state;
+//     return (
+//       <div className='boxpesquisa'>
+//         <label>PESQUISAR</label>
+//         <input className='designImput' onChange={(e) => this.pesquisar(e.target.value)} />
+
+//         <h1>Lista de Personagens:</h1>
+
+//         <ul className="personagens">
+//           {personagens.map((item) => (
+//             <PersonagemDetail
+//               key={item.people}
+//               personagem={item}
+//             />
+//           ))}
+//         </ul>
+//       </div>
+//     );
+//   }
+// }
